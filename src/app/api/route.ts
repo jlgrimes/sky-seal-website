7a9511d9d1adfd8dca115528cb6c3ca6f357b783
BuildTools <unconfigured@null.spigotmc.org>
2023-09-05 10:39:31 -0500

    added get just for kicks

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -2,6 +2,42 @@ import { NextResponse } from 'next/server';
 import { cookies } from 'next/headers';
 import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs';
 import { normalizeDeckList } from './_helpers';
+
+export async function GET(request: Request) {
+  const supabase = createRouteHandlerClient({ cookies })
+  const { searchParams } = new URL(request.url);
+  const deckId = searchParams.get('id');
+
+  if (!deckId) return NextResponse.json({
+    error: 'incorrect-query-params',
+    message: 'Incorrect query parameters.',
+    details: 'Ensure you are passing in the param for id.'
+  }, { status: 400 });
+
+  if (deckId.length !== 6) return NextResponse.json({
+    error: 'incorrect-query-params',
+    message: 'Incorrect query parameters.',
+    details: 'Deck id must have a length of 6.'
+  }, { status: 400 });
+
+  const deck = await supabase.from('decks').select('frozen_list').eq('id', deckId);
+
+  if (deck.error) return NextResponse.json({
+    error: 'database-error',
+    message: deck.error.message,
+    details: deck.error.details
+  }, { status: 400 });
+
+  if (deck.data && deck.data.length === 0) return NextResponse.json({
+    error: 'deck-not-found',
+    message: 'Deck not found.',
+    details: 'No deck found with that id.'
+  }, { status: 400 });
+
+  return NextResponse.json({
+    list: deck.data[0].frozen_list
+  });
+}
  
 export async function POST(request: Request) {
   const supabase = createRouteHandlerClient({ cookies })
