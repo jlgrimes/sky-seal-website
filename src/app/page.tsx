a10b2dd9fb18e6d73b7f92b8df46a13a0b8d89d8
BuildTools <unconfigured@null.spigotmc.org>
2023-09-14 18:25:52 -0500

    slooppy page + key frames done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,13 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <main className={styles.main}>
+      <div className={styles.title}>
+        <h1 className={styles.concealed}>concealed<span className={styles.dotCards}>.cards</span></h1>
+        <h2>The future of deck building.</h2>
+      </div>
       <div className={styles.card}>
-        <h2>
-            Nothing here... yet
+          <h2>
+            Coming soon.
           </h2>
           <p>Get it? Concealed cards. Like the ability? Haha</p>
           <p>Stay tuned. Follow @jgrimesey.</p>
