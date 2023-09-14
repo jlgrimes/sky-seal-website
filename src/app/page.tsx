60709dc3a8d664f9cf154659b96a5c3e70be0ecf
BuildTools <unconfigured@null.spigotmc.org>
2023-09-14 12:34:47 -0500

    added notes on the website lol

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,12 @@ export default function Home() {
           <p>Get it? Concealed cards. Like the ability? Haha</p>
           <p>Stay tuned. Follow @jgrimesey.</p>
       </div>
+      <div className={styles.card}>
+        <h2>
+            Stuff to put in API docs:
+          </h2>
+          <p>User decks have 10 digit IDs. Temp decks have 11 digit IDs.</p>
+      </div>
     </main>
   )
 }
